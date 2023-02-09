c2b5d74f2ca63f9d6aebb276faf6828e296d9a94
Ali <alistraker17@msn.com>
2023-02-09 23:29:22 +0000

    got 'get rider list' working

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs #v4.11.1
 import requests
 import os
+from pathlib import Path, WindowsPath
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -47,47 +48,36 @@ def get_rider_info(tin_of_soup):
     var = tin_of_soup
 
 
-def get_team_list():
-    #go to basically any pcs page and get the team list:
-    """
-    <li class="more4"><a class="reg" href="teams.php">Teams</a><a class="more4 toggleNavDropdown" href="">▼</a><ul class="hide"><li><a href="team/bora-hansgrohe-2023">BORA - hansgrohe</a></li>
-    <li><a href="team/cofidis-2023">Cofidis </a></li>
-    <li><a href="team/ef-education-easypost-2023">EF Education-EasyPost</a></li>
-    <li><a href="team/groupama-fdj-2023">Groupama - FDJ</a></li>
-    <li><a href="team/ineos-grenadiers-2023">INEOS Grenadiers</a></li>
-    <li><a href="team/intermarche-circus-wanty-2023">Intermarché - Circus - Wanty </a></li>
-    <li><a href="team/team-jumbo-visma-2023">Jumbo-Visma</a></li>
-    <li><a href="team/movistar-team-2023">Movistar Team</a></li>
-    <li><a href="team/soudal-quick-step-2023">Soudal - Quick Step</a></li>
-    <li><a href="team/team-arkea-samsic-2023">Team Arkéa Samsic</a></li>
-    <li><a href="team/team-jayco-alula-2023">Team Jayco AlUla</a></li>
-    <li><a href="team/team-dsm-2023">Team DSM</a></li>
-    <li><a href="team/trek-segafredo-2023">Trek - Segafredo</a></li>
-    <li><a href="team/ag2r-citroen-team-2023">AG2R Citroën Team</a></li>
-    <li><a href="team/uae-team-emirates-2023">UAE Team Emirates</a></li>
-    <li><a href="team/alpecin-deceuninck-2023">Alpecin-Deceuninck</a></li>
-    <li><a href="team/astana-qazaqstan-team-2023">Astana Qazaqstan Team</a></li>
-    <li><a href="team/bahrain-victorious-2023">Bahrain - Victorious</a></li>
-    </ul></li>
+def get_team_list(root_page_addr: str) -> list:
+    # go to basically any pcs page and get the team list:
+    team_list = []
+    sp = get_soup(root_page_addr)
+    var = sp.html.body.find_all('li')
 
-    :return:
-    """
+    for li in var:
+        if len(li.contents) > 0 and li.contents[0].string == 'Teams':
+            new_li = li
+            break
+
+    var = new_li.find_all('a')
+    for li in var:
+        if li.string != 'Teams' and not 'class' in li.attrs:
+            team_list.append(li.string)
+
+    print(team_list)
+    return team_list
+
 
 def get_soup(address_str):
 
     page = requests.get(address_str)
-    print(type(page))
-    print(page)
-    return bs(page.content)
+    return bs(page.content, features="html.parser")
 
 def get_and_save_raw_html_soup(address_str, dir):
-    #function to save the raw scraped pages as reference
-    file_name = address_str.split('/')[-1] + '.txt'
+    # function to save the raw scraped pages as reference
+    file_name = address_str.split('/')[-1] + '.html'
     page = requests.get(address_str)
     page.encoding = page.apparent_encoding
-    print(page.apparent_encoding)
-    #print(os.listdir(dir))
-    page.encoding = "utf-8"
     with open(os.path.join(dir, file_name), 'w', encoding="utf-8") as fd:
         fd.write(page.text)
     success = True
@@ -95,12 +85,29 @@ def get_and_save_raw_html_soup(address_str, dir):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data_dir = "../ProCyclingDEP_Data/Riders/"
+    repo_dir = Path.cwd()
+    storage_dir = repo_dir.parent.joinpath(WindowsPath('ProCyclingDE_Data'))
+    riders_storage_dir = storage_dir.joinpath(WindowsPath('riders'))
+    misc_storage_dir = storage_dir.joinpath(WindowsPath('misc'))
+
+    root_page_addr = 'https://www.procyclingstats.com/'
     addr = 'https://www.procyclingstats.com/rider/wout-van-aert'
     soup = get_soup(addr)
-    stat = get_and_save_raw_html_soup(addr, data_dir)
-    print_hi('PyCharm')
+
+    get_team_list(root_page_addr)
+
+
+
+    #get_and_save_raw_html_soup(root_page_addr, misc_storage_dir)
+    #if not misc_storage_dir.is_dir():
+        #misc_storage_dir.mkdir()
+
+    #data_dir = Path("../ProCyclingDEP_Data/Riders/")
+
+
+    #stat = get_and_save_raw_html_soup(addr, misc_storage_dir)
+    #print_hi('PyCharm')
     #init_scrape_site(address_str)
-    get_rider_websites(soup)
+    #get_rider_websites(soup)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
